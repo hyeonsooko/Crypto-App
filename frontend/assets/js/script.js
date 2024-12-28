4044c36497f998a70325eecfa56417ff6dbeaa1e
Hyeonsoo Ko <hko@uab.edu>
2024-12-27 21:06:40 -0600

    frontend change

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -111,7 +111,7 @@ function displayTrendCoins(coins) {
             <td>${coinData.data.total_volume}</td>
             <td class="${coinData.data.price_change_percentage_24h.usd >= 0 ? 'green' : 'red'}">${coinData.data.price_change_percentage_24h.usd.toFixed(2)}%</td>
         `;
-        row.onclick = () => window.location.href = `../../pages/coin.html?coin=${coinData.id}`;
+        row.onclick = () => window.location.href = `../../frontend/pages/coin.html?coin=${coinData.id}`;
         table.appendChild(row);
     });
     coinsList.appendChild(table);
@@ -160,7 +160,7 @@ function displayAssets(data){
             sparkline: asset.sparkline_in_7d.price,
             color: asset.sparkline_in_7d.price[0] <= asset.sparkline_in_7d.price[asset.sparkline_in_7d.price.length - 1] ? 'green' : 'red',
         });
-        row.onclick = () => window.location.href = `../../pages/coin.html?coin=${asset.id}`
+        row.onclick = () => window.location.href = `../../frontend/pages/coin.html?coin=${asset.id}`
     });
     cryptoList.appendChild(table);
 
